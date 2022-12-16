f698ce978eabbbfd3b1b3a80da48a1d029be7ea3
Pboat <49327649+Pboat@users.noreply.github.com>
2022-12-16 12:45:19 -0500

    Add files via upload

---
+++ C++/math2Num/math2Num/Assignment-3B.cpp
@@ -0,0 +1,26 @@
+/**
+Name: Daniel Bruce
+Date:9/6/2018
+Class:CIS2541 - NET01 C++ Language Programming
+Description: Takes a user's numerical input three times and then displays
+  the results of various mathematical functions using that input
+**/
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//create variables
+	float firstInput, secondInput, thirdInput = 0;
+
+	cout << "Math! Now With Three Numbers!" << endl;
+	//ask for input
+	cout << "Please enter three numbers: ";
+	cin >> firstInput >> secondInput;
+
+	//calculate, then communicate
+	
+	return 0;
+}
